7504e265ec81956ff7f7085b56a0c0682995d849
noahhipp <noahhipp@googlemail.com>
2020-10-14 13:06:31 +0200

    firstlevel no running, mainly do to killing eoi stuff

---
+++ 03_firstlevel/rmspmt.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Move to data 
+cd /projects/crunchie/hipp/wavepain
+
+# Collect folders
+folders=$(ls -d sub*/)
+run_folders="fir"
+
+for folder in $folders; do
+    # Move in 
+    cd $folder    
+    echo "__________________
+    $folder"
+    
+    for run_folder in $run_folders; do
+	    if [[ "sub010/" == *$folder* ]];
+            then
+              continue 
+    	    fi
+	    cd $run_folder
+	    echo "$run_folder"
+
+        # Do stuff
+	    rm  spmT*  -r -f	    I
+	    cd ../ # leave run_folder
+    done
+    echo  "________________
+    "
+    cd ../ # leave sub_folder
+    
+done
+
