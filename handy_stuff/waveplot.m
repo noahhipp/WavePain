67b49fd3e7d30c00a7c6999d7516cff635431211
noahhipp <noahhipp@googlemail.com>
2020-11-11 16:12:45 +0100

    something

--- handy_stuff/waveplot.m
+++ handy_stuff/waveplot.m
@@ -9,6 +9,8 @@ function line = waveplot(y, condition, varargin)
 %           - varargin{2} specifies number of samples taken into account for wave
 %           calculation otherwise defaults to numel(y)
 
+index_test = 0;
+
 % E.g. waveplot(1:110, ones(1,110), 'M21')
 n_wave      = numel(y);
 n           = n_wave;
@@ -19,19 +21,32 @@ if nargin > 2
 end
 
 % No task, 1back, 2back
-colors = [0 0 0;...
-          0 0 1;...
-          1 0 0]; % replace with fancy seaborn colors later
+colors = [0 0 0;... % no task
+          0 0 1;... % 1back         
+          1 0 0;... % 2back
+          0 1 1;... % 2back-1back   
+          1 1 0];   % 1back-2back
     
 [~,ticks] = getBinBarPos(n_wave);
 
 x               = linspace(0,120,n); % used for plotting
 xx              = 1:n; % indices of x, used for constructing index table
 ind             = table;
-ind.pre_task    = xx < ticks(2);
-ind.task1       = xx >= ticks(2) & xx < ticks(4);
-ind.task2       = xx >= ticks(4) & xx < ticks(6);
+ind.pre_task    = xx <= ticks(2)+1.2;
+ind.task1       = xx >= ticks(2)+.5 & xx <= ticks(4)+1;
+ind.task2       = xx >= ticks(4) & xx <= ticks(6)+1;
 ind.post_task   = xx >= ticks(6);
+
+if index_test
+    figure;
+    for i = 1:4        
+        plot(ind{:,i});
+        hold on;
+        ylim([-1.5,1.5]);
+    end
+end
+     
+
 
 % Specify color sequence according to condition
 switch condition
@@ -41,12 +56,18 @@ switch condition
     case 'M12'
         cs = [1, 2, 3, 1];
         legend_labels = {'no task', '1back', '2back'};
+    case 'M21vsM12'
+        cs = [1, 4, 5, 1];
+        legend_labels = {'no task', '2back-1back', '1back-2back'};
     case 'W21'
         cs = [1, 3, 2, 1];
         legend_labels = {'no task', '2back', '1back'};
     case 'W12'
         cs = [1, 2, 3, 1];        
-        legend_labels = ['no task', '2back', '1back'];
+        legend_labels = {'no task', '2back', '1back'};
+    case 'W21vsW12'
+        cs = [1, 4, 5, 1];
+        legend_labels = {'no task', '2back-1back', '1back-2back'};
     case 'Monline'
         cs = ones(1,4);
         legend_labels = {'no task'};
