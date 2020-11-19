78b0d2a8ca4f137e5dbd1028e17990baa43d9a76
noahhipp <noahhipp@googlemail.com>
2020-11-19 14:07:06 +0100

    slimed children and mom

--- 04_secondlevel/callback_mom.m
+++ 04_secondlevel/callback_mom.m
@@ -1,20 +1,8 @@
-function callback_mom(child)
-% sets specified function (child...) to be invoked by updating st.centre
+function callback_mom
 
-switch child
-    case 1
-        f = @child1_plot_f_contrasts_baseline_corrected;
-        m = 'spm_plot_f_constrats_baseline_corrected.m';
-    case 2
-        f = @child2_plot_f_contrasts_eoi;
-        m = 'eoi of all 6 conditions';
-    otherwise
-        m = 'NOTHING';                
-end
+child1_plot_f_contrasts_baseline_corrected;
+
+child2_plot_f_contrasts_eoi;
 
 
-% Set callback
-global st
-st.callback = f;
 
-fprintf('\n CAVE: %s set as callback function for st.centre\n', m);
